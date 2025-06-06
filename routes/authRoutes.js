d58a91798c1f2609dbac200031368077aa1855b0
henryyr <henry.360420@gmail.com>
2025-06-06 22:12:54 +0800

    test promo

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const { authenticate } = require('../middleware/auth');
 const authController = require('../controllers/user/authController');
-const { forgotLimiter } = require('../config/rateLimit');
 const router = express.Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authenticate, authController.logout);
 router.get('/profile', authenticate, authController.getProfile);
-router.post('/forgot-password', forgotLimiter, authController.forgotPassword);
+router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
