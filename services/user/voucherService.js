a2c605a0b2b2bbfc58e7c4c9b9e014693183d6dc
henryyr <henry.360420@gmail.com>
2025-06-08 00:00:31 +0800

    test4

--- services/user/voucherService.js
+++ services/user/voucherService.js
@@ -1,16 +1,22 @@
+const { pool } = require("../../db");
+
 const validateVoucher = async (code) => {
   const [vouchers] = await pool.query(
     `SELECT * FROM vouchers WHERE code = ? AND is_active = 1`,
     [code]
   );
-  if (vouchers.length === 0) throw new Error("Voucher tidak valid");
-  const voucher = vouchers[0];
 
+  if (vouchers.length === 0) throw new Error("Voucher tidak valid");
+
+  const voucher = vouchers[0];
   const now = new Date();
+
   if (voucher.valid_from && now < new Date(voucher.valid_from))
     throw new Error("Voucher belum berlaku");
+
   if (voucher.valid_until && now > new Date(voucher.valid_until))
     throw new Error("Voucher sudah kedaluwarsa");
+
   if (voucher.usage_limit !== null && voucher.used_count >= voucher.usage_limit)
     throw new Error("Voucher sudah habis digunakan");
 
@@ -22,4 +28,8 @@ const validateVoucher = async (code) => {
     valid_from: voucher.valid_from,
     valid_until: voucher.valid_until,
   };
+};
+
+module.exports = {
+  validateVoucher,
 };
